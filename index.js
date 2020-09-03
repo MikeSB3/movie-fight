bdcd53a3800991162797039e6b7b446fad550186
Michael Berryman <berryman.michael@yahoo.com>
2020-09-03 10:36:02 -0700

    add search option

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const fetchData = async () => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: 'a169202f',
-            i: 'tt0848228'
-        }
-    });
-    console.log(response.data);
-}
+const fetchData = async (searchTerm) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params : {
+			apikey : 'a169202f',
+			s      : searchTerm
+		}
+	});
+	console.log(response.data);
+};
 
-fetchData();
+const input = document.querySelector('input');
+
+let timeoutId;
+const onInput = (event) => {
+	if (timeoutId) {
+		clearTimeout(timeoutId);
+	}
+	timeoutId = setTimeout(() => {
+		fetchData(event.target.value);
+	}, 500);
+};
+
+input.addEventListener('input', onInput);
